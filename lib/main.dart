eb873d73674325d3bc860e92e49c2528453fdc37
Ahmad Ruslan <aruslan8395@gmail.com>
2020-03-24 15:59:21 +0700

    Custom list view

--- lib/main.dart
+++ lib/main.dart
@@ -29,25 +29,28 @@ class BodyLayout extends StatelessWidget {
 }
 
 Widget _myListView(BuildContext context) {
-
-  final titles = ['bike', 'boat', 'bus', 'car',
-    'railway', 'run', 'subway', 'transit', 'walk'];
-
-  final icons = [Icons.directions_bike, Icons.directions_boat,
-    Icons.directions_bus, Icons.directions_car, Icons.directions_railway,
-    Icons.directions_run, Icons.directions_subway, Icons.directions_transit,
-    Icons.directions_walk];
-
+  Widget column = Expanded(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('title', style: TextStyle(fontSize: 16),),
+        Text('Subtitle')
+      ],
+    ),
+  );
   return ListView.builder(
-    itemCount: titles.length,
     itemBuilder: (context, index) {
       return Card(
-        child: ListTile(
-          leading: Icon(icons[index]),
-          title: Text(titles[index]),
-          trailing: Icon(Icons.keyboard_arrow_right),
-        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              column,
+              column,
+            ],
+          )
+        )
       );
-    },
+    }
   );
 }
